4c06a7b78f01317aa29a227be5501826f6f6ee58
Kevin Karma <kevinkarma55@gmail.com>
2019-08-16 03:17:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(file)
   result["get_emoticon"] = {}
   code.each do |meaning, emoticons|
     result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
   end
   result
 end
