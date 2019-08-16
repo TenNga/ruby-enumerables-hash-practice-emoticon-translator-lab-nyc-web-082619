ea37544405c786297b5589e1a7ab2532f30b37dd
Kevin Karma <kevinkarma55@gmail.com>
2019-08-16 03:20:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,8 @@ def load_library(file)
   result
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yamlFile, emoticon)
+  load_library(yamlFile)
 end
 
 def get_english_meaning
